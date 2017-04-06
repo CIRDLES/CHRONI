08634366f842a35913d22a3f2a2ab7baf8b6889e
Drew Bigelow <rdbigelow@gmail.com>
2017-04-06 12:54:24 -0400

    Added history button functionality

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -1,8 +1,14 @@
 import { Component, Pipe } from '@angular/core';
 import { HistoryUtility, HistoryEntry } from '../../utilities/HistoryUtility';
 
-import { Platform } from 'ionic-angular';
-// import { ScreenOrientation, File } from 'ionic-native';
+import { ScreenOrientation, File } from 'ionic-native';
+
+import { XMLUtility, Aliquot, ReportSettings } from '../../utilities/XMLUtility';
+import { FileBrowser } from '../fileBrowser/fileBrowser';
+import { TableView } from '../table/tableView';
+import { FileUtility } from '../../utilities/FileUtility';
+import { NavController, Platform, ModalController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 declare var cordova: any;
 
@@ -13,8 +19,9 @@ export class History {
 
 	fileSystem: string;
 	public history: Array<HistoryEntry> = [];
+    public historyIndex: HistoryEntry;
 
-    constructor(public platform: Platform, public historyUtil: HistoryUtility) {
+    constructor(public platform: Platform, public historyUtil: HistoryUtility, public navCtrl: NavController, public xml: XMLUtility, public fileUtil: FileUtility, public modalCtrl: ModalController, public storage: Storage) {
     	this.platform.ready().then(() => {
             this.fileSystem = cordova.file.dataDirectory;
         });
@@ -27,6 +34,29 @@ export class History {
         // this.platform.ready().then((val) => {
         //     ScreenOrientation.lockOrientation('portrait').catch((error) => console.log("Orientation Lock Error: " + error));
         // });
+    }
+
+    openTable(i) {
+        
+        this.historyIndex = this.historyUtil.getHistoryEntry(i);
+        this.xml.createAliquot(this.historyIndex.getAliquotFile()).subscribe(al => {
+            if (al) {
+                var aliquot: Aliquot = <Aliquot> al;
+                this.xml.createReportSettings(this.historyIndex.getReportSettingsFile()).subscribe(rs => {
+                    if (rs) {
+                        var reportSettings: ReportSettings = <ReportSettings> rs;
+                        var tableArray = this.xml.createTableData(aliquot, reportSettings);
+                        var entry = new HistoryEntry(this.historyIndex.getAliquotFile(), this.historyIndex.getReportSettingsFile(), new Date());
+                        this.historyUtil.addEntry(entry);
+                        this.navCtrl.push(TableView, {
+                            tableArray: tableArray,
+                            aliquot: this.historyIndex.getAliquotFile(),
+                            reportSettings: this.historyIndex.getReportSettingsFile()
+                        });
+                    }
+                });
+            }
+        });
     }
 
 }
