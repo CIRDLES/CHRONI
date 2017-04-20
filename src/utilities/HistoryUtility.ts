9247750d82f7c3dafea5fcbfd6c78ff282c9690b
Tyler Newman <tnewman16@gmail.com>
2017-04-20 15:56:30 -0400

    Removes Some Console Logs

--- src/utilities/HistoryUtility.ts
+++ src/utilities/HistoryUtility.ts
@@ -32,8 +32,7 @@ export class HistoryUtility {
     public addEntry(entry: HistoryEntry) {
         var place = 0;
         var index = -1;
-        console.log(entry.getAliquotFile());
-        console.log(entry.getReportSettingsFile());
+
         for (let x of this.historyEntries)
         {
             if (entry.getAliquotFile().name === x.getAliquotFile().name && entry.getReportSettingsFile().name === x.getReportSettingsFile().name) {
